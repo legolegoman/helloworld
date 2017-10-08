4efdf3904c89289723189f473b48f4c7c76f14ea
wongchunhung <chunha@gmail.com>
2017-10-08 12:33:29 +0800

    Update my_script.py

--- my_script.py
+++ my_script.py
@@ -1,4 +1,8 @@
+# update at 8 Oct 17
+# get the pulblic IP address
+
 import requests
+
 
 response = requests.get('https://httpbin.org/ip')
 print('Your IP is {0}'.format(response.json()['origin']))
